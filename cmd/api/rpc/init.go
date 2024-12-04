1c0a099233bc9da77449fbe4c72c43676d062603
DanceRain <1477364283@qq.com>
2024-12-04 17:33:47 +0800

    “重构了服务发现”

--- cmd/api/rpc/init.go
+++ cmd/api/rpc/init.go
@@ -4,20 +4,24 @@ import (
 	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/resolver"
 	"wgxDouYin/grpc/user"
 	"wgxDouYin/pkg/etcd"
 	"wgxDouYin/pkg/viper"
 )
 
 func init() {
+
 	userConfig := viper.Init("user")
 	InitUser(&userConfig)
 }
 
 // initClient this function connect to rpc server by service name. service name will be
 // resolved by resolver that is init by InitUser.
-func initClient(scheme, serviceName string, client interface{}) {
-	conn, err := connectServer(scheme, serviceName)
+func initClient(etcdAddress []string, serviceName string, client interface{}) {
+	tikTokResolver := &etcd.TikTokServiceResolverBuilder{}
+	tikTokResolver.EtcdAddresses = etcdAddress
+	conn, err := connectServer(tikTokResolver, serviceName)
 	if err != nil {
 		panic(err)
 	}
@@ -30,16 +34,13 @@ func initClient(scheme, serviceName string, client interface{}) {
 }
 
 // connectServer is used by initClient.
-func connectServer(scheme, serviceName string) (conn *grpc.ClientConn, err error) {
+func connectServer(resolverBuilder *etcd.TikTokServiceResolverBuilder, serviceName string) (conn *grpc.ClientConn, err error) {
+	resolver.Register(resolverBuilder)
 	opts := []grpc.DialOption{
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 	}
-	addr := fmt.Sprintf("%s:///%s", scheme, etcd.AddrPrefix(serviceName))
+	addr := fmt.Sprintf("%s:///%s", resolverBuilder.Scheme(), etcd.AddrPrefix(serviceName))
 	fmt.Printf("connect addr %v\n", addr)
 	conn, err = grpc.NewClient(addr, opts...)
 	return
-}
-
-func initPublicKey(scheme, serviceName string) {
-
 }
--- cmd/api/rpc/user.go
+++ cmd/api/rpc/user.go
@@ -3,10 +3,8 @@ package rpc
 import (
 	"context"
 	"fmt"
-	"google.golang.org/grpc/resolver"
 	"wgxDouYin/grpc/user"
 	grpc "wgxDouYin/grpc/user"
-	"wgxDouYin/pkg/etcd"
 	"wgxDouYin/pkg/viper"
 )
 
@@ -15,13 +13,9 @@ var (
 )
 
 func InitUser(config *viper.Config) {
-	etcdAddr := fmt.Sprintf("%s:%d", config.Viper.GetString("etcd.host"), config.Viper.GetInt("etcd.port"))
+	etcdAddresses := []string{fmt.Sprintf("%s:%d", config.Viper.GetString("etcd.host"), config.Viper.GetInt("etcd.port"))}
 	serviceName := config.Viper.GetString("server.name")
-	Discoverer := etcd.NewDiscoverer([]string{etcdAddr})
-
-	resolver.Register(Discoverer)
-	//defer Discoverer.Close()
-	initClient(Discoverer.Scheme(), serviceName, &userClient)
+	initClient(etcdAddresses, serviceName, &userClient)
 }
 
 func Register(ctx context.Context, req *user.UserRegisterRequest) (*user.UserRegisterResponse, error) {
